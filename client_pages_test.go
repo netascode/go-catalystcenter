59665af02955d0c4d5b257b68de9b8d48b73c6e9
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2024-03-02 12:28:57 +0100

    add TestClientGet_Concurrent

--- client_pages_test.go
+++ client_pages_test.go
@@ -1,6 +1,7 @@
 package cc
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -153,4 +154,153 @@ func TestClientGet_PageTooBig(t *testing.T) {
 	res, err := client.Get("/url")
 	assert.NoError(t, err)
 	assert.Equal(t, `{"response":["1","2","3","4"]}`, res.Raw)
+}
+
+// TestClientGet_Concurrent test against a concurrent Post/Put/Delete modifying how data
+// is divided into the pages. As Get glues the desired pages, the modifying methods shouldn't
+// interrupt.
+func TestClientGet_Concurrent(t *testing.T) {
+	defer gock.Off()
+	client := authenticatedTestClient()
+
+	// For pagination tests to be readable, we use dummy page size of 3 instead of 500.
+	// Since we are changing a package-level var, this test cannot be run on t.Parallel().
+	maxItems = 3
+
+	recorder := make(chan string)
+	cleanup := make(chan bool)
+
+	// Stochastic test: the more, the merrier.
+	const (
+		posters  = 10
+		putters  = 10
+		deleters = 10
+		taskers  = 10
+		getters  = 1
+	)
+
+	gock.New(testURL).Post("/url/insert").
+		Times(posters).
+		Reply(200).
+		Map(func(resp *http.Response) *http.Response {
+			recorder <- "post"
+			return resp
+		})
+
+	for i := 0; i < posters; i++ {
+		go func() {
+			_, err := client.Post("/url/insert", "{}")
+			assert.NoError(t, err)
+			cleanup <- true
+		}()
+	}
+
+	gock.New(testURL).Put("/url").MatchParam("id", "5").
+		Times(putters).
+		Reply(200).
+		Map(func(resp *http.Response) *http.Response {
+			recorder <- "put"
+			return resp
+		})
+
+	for i := 0; i < putters; i++ {
+		go func() {
+			_, err := client.Put("/url?id=5", "{}")
+			assert.NoError(t, err)
+			cleanup <- true
+		}()
+	}
+
+	gock.New(testURL).Delete("/url/5").
+		Times(deleters).
+		Reply(200).
+		Map(func(resp *http.Response) *http.Response {
+			recorder <- "delete"
+			return resp
+		})
+
+	for i := 0; i < deleters; i++ {
+		go func() {
+			_, err := client.Delete("/url/5")
+			assert.NoError(t, err)
+			cleanup <- true
+		}()
+	}
+
+	gock.New(testURL).Post("/taskable").
+		Times(taskers).
+		Reply(200).
+		BodyString(`{"response": {"taskId": "123"}}`)
+	gock.New(testURL).Get("/api/v1/task/123").
+		Times(taskers).
+		Reply(200).
+		BodyString(`{"response": {"endTime": "1", "isError": false}}`).
+		Map(func(resp *http.Response) *http.Response {
+			recorder <- "task"
+			return resp
+		})
+
+	for i := 0; i < taskers; i++ {
+		go func() {
+			_, err := client.Post("/taskable", "{}")
+			assert.NoError(t, err)
+			cleanup <- true
+		}()
+	}
+
+	// GET that glues 3 pages
+	go func() {
+		e := struct{}{}
+		gock.New(testURL).Get("/url").
+			Reply(200).
+			JSON(map[string]any{
+				"response": []any{e, e, e},
+			}).
+			Map(func(resp *http.Response) *http.Response {
+				recorder <- "get"
+				return resp
+			})
+		gock.New(testURL).Get("/url").MatchParam("offset", "4").
+			Reply(200).
+			JSON(map[string]any{
+				"response": []any{e, e, e},
+			}).
+			Map(func(resp *http.Response) *http.Response {
+				recorder <- "get"
+				return resp
+			})
+		gock.New(testURL).Get("/url").MatchParam("offset", "7").
+			Reply(200).
+			JSON(map[string]any{
+				"response": []any{e, e},
+			}).
+			Map(func(resp *http.Response) *http.Response {
+				recorder <- "get"
+				return resp
+			})
+		res, err := client.Get("/url")
+		assert.NoError(t, err)
+		assert.Equal(t, `{"response":[{},{},{},{},{},{},{},{}]}`+"\n", res.Raw)
+		cleanup <- true
+	}()
+
+	got := ""
+	// Serialize writing the result, all the requests wait their turn.
+	// Record the sequence of events.
+	go func() {
+		for v := range recorder {
+			got = got + "," + v
+		}
+		cleanup <- true
+	}()
+
+	// Do not leak goroutines; do not leak t.Error calls made from inside goroutines.
+	for i := 0; i < getters+posters+putters+deleters+taskers; i++ {
+		<-cleanup
+	}
+
+	close(recorder)
+	<-cleanup // and the serializer itself
+
+	assert.Contains(t, got, "get,get,get")
 }
